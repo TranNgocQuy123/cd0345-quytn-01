33b2ce218a1e4ee77473d2dd9bb903c7a0975998
TranNgocQuy123 <44964317+TranNgocQuy123@users.noreply.github.com>
2024-07-30 02:21:46 +0000

    Update variables.tf

--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,18 @@
 # TODO: Define the variable for aws_region
+variable "region" {
+  type        = string
+  description = "region to deploy instance"
+  default     = "us-east-1"
+}
+
+variable "lambda_name" {
+  type        = string
+  description = "lambda name"
+  default     = "greet_lambda"
+}
+
+variable "lambda_function_payload" {
+  type        = string
+  description = "lambda function payload"
+  default = "lambda_function_payload.zip"
+}
