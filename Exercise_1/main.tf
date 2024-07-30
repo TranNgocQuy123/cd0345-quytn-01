5a21cb1750957979fa615078a35d2df08dc45d48
TranNgocQuy123 <44964317+TranNgocQuy123@users.noreply.github.com>
2024-07-30 01:32:37 +0000

    Update main.tf
    delete the 2 m4.large instances.

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -21,11 +21,11 @@ resource "aws_instance" "Udacity_T2" {
   }
 }
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
-resource "aws_instance" "Udacity_M4" {
-  count         = var.ec2_instance_count[1]
-  ami           = var.ec2_ami_id
-  instance_type = var.instance_type[1]
-  tags = {
-    Name = "Udacity M4"
-  }
-}
+# resource "aws_instance" "Udacity_M4" {
+#   count         = var.ec2_instance_count[1]
+#   ami           = var.ec2_ami_id
+#   instance_type = var.instance_type[1]
+#   tags = {
+#     Name = "Udacity M4"
+#   }
+# }
